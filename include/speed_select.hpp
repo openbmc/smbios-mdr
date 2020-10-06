bac3fa9aa9406e4086d366a76442a5eaf224405c
Jonathan Doman <jonathan.doman@intel.com>
2020-10-05 23:25:45 -0700

    cpuinfoapp: Add SST discovery feature
    Retrieve Intel Speed Select Technology (SST) configuration values for
    all ICX CPUs via PECI (OS-PCode mailbox). Each CPU may have up to three
    Performance Profiles (PP), each with accompanying Base Frequency (BF)
    information.
    
    The profile data is published on D-Bus using two predefined interfaces:
     - xyz.openbmc_project.Control.Processor.CurrentOperationConfig, which
       is implemented on each "cpu" object in the inventory, and contains
       mutable properties for OOB configuration (modifiying properties not
       supported yet).
     - xyz.openbmc_project.Inventory.Item.Cpu.OperationConfig, which is
       implemented on separate "config" objects and contains the readonly
       properties for each performance profile.
    
    Tested:
     - Profiled performance of PECI operations via code instrumentation
       (takes ~2 min per CPU during BMC boot, ~2 sec during BMC idle).
     - Inspected D-Bus objects using busctl
     - Inspected Redfish objects created with corresponding change to
       bmcweb.
     - Injected PECI failures in code to test error handling.
    
    Change-Id: I0d8ae79655dfd2880cf3bae6abe600597740df7c
    Signed-off-by: Jonathan Doman <jonathan.doman@intel.com>

---
+++ include/speed_select.hpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+#include <sdbusplus/asio/connection.hpp>
+#include <boost/asio/io_context.hpp>
+
+/**
+ * Retrieve all SST configuration info for all discoverable CPUs, and publish
+ * the info on new D-Bus objects on the given bus connection.
+ *
+ * This function may block until all discovery is completed (many seconds), or
+ * it may schedule the work to be done at a later time (on the given ASIO
+ * context) if CPUs are not currently available, and will also schedule periodic
+ * work to be done after initial discovery is completed.
+ *
+ * @param[in,out]   ioc     ASIO IO context/service
+ * @param[in,out]   conn    D-Bus ASIO connection.
+ */
+void getSSTConfigInfo(boost::asio::io_context& ioc,
+                      std::shared_ptr<sdbusplus::asio::connection> conn);
