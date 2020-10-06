a2f035c28ea5fb1101a97511ef4c6408f58ab232
Jonathan Doman <jonathan.doman@intel.com>
2020-10-05 23:25:45 -0700

    cpuinfoapp: Add SST discovery feature
    Retrieve Intel Speed Select Technology (SST) configuration values for
    all CPUs via PECI (OS-PCode mailbox). Each CPU may have up to three
    Performance Profiles (PP), each with accompanying Base Frequency (BF)
    and Turbo Frequency (TF) information.
    
    The profile data is published on D-Bus using two predefined interfaces:
     - xyz.openbmc_project.Control.Processor.CurrentOperationConfig, which
       is implemented on each "cpu" object in the inventory, and contains
       mutable properties for OOB configuration (modifiying properties not
       supported yet).
     - xyz.openbmc_project.Inventory.Item.Cpu.OperationConfig, which is
       implemented on separate "config" objects and contains the readonly
       properties for each performance profile.
    
    Tested:
     - Profiled performance of PECI operations via code instrumentation
       (takes ~2 min per CPU during BMC boot, ~2 sec during BMC idle).
     - Inspected D-Bus objects using busctl
     - Inspected Redfish objects created with corresponding change to
       bmcweb.
     - Injected PECI failures in code to test error handling.
    
    Change-Id: I0d8ae79655dfd2880cf3bae6abe600597740df7c
    Signed-off-by: Jonathan Doman <jonathan.doman@intel.com>

--- src/cpuinfo_main.cpp
+++ src/cpuinfo_main.cpp
@@ -15,6 +15,7 @@
 */
 
 #include "cpuinfo.hpp"
+#include "speed_select.hpp"
 
 #include <errno.h>
 #include <fcntl.h>
@@ -44,8 +45,6 @@ namespace phosphor
 namespace cpu_info
 {
 
-static constexpr const char* cpuPath =
-    "/xyz/openbmc_project/inventory/system/chassis/motherboard/cpu";
 static constexpr const char* cpuInterfaceName =
     "xyz.openbmc_project.Inventory.Decorator.Asset";
 static constexpr const char* cpuProcessName =
@@ -203,7 +202,7 @@ static void createCpuUpdatedMatch(
         sdbusplus::bus::match::rules::interfacesAdded() +
             sdbusplus::bus::match::rules::argNpath(0, objectPath.c_str()),
         [&conn, cpu, propValues](sdbusplus::message::message& msg) {
-            std::string objectName;
+            sdbusplus::message::object_path objectName;
             boost::container::flat_map<
                 std::string,
                 boost::container::flat_map<std::string,
@@ -357,6 +356,7 @@ static void
         // get the PECI client address list
         getPECIAddrMap(cpuMap);
         getProcessorInfo(conn, objServer, cpuMap);
+        getSSTConfigInfo(io, conn);
     }
     if (!peciAvailable || !cpuMap.size())
     {
